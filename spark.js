b20872909ccfbb0c4d7593513433210ba2878218
Bret Comnes <bcomnes@gmail.com>
2019-07-31 12:57:59 +0200

    Fix bar chart

--- spark.js
+++ spark.js
@@ -11,13 +11,17 @@ process.stdin
   .pipe(process.stdout)
 
 let max = 0
+const barLength = 15
 
 function map (line) {
   const s = speed(1)
   if (s > max) max = s
   const percentage = s / max
-  const proportion = (percentage * 10).toFixed(0)
-  const bar = (new Array(10)).map((segment, i) => i <= proportion ? '█' : '░')
+  const proportion = (percentage * barLength).toFixed(0)
+  const bar = []
+  for (let index = 0; index < barLength; index++) {
+    bar[index] = index <= proportion ? '█' : '░';
 
-  return bar + ' - ' + line + EOL
+  }
+  return bar.join('') + ' - ' + line + EOL
 }
